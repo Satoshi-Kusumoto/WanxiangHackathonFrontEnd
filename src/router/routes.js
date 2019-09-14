af37607995dc6af587ed429308e457febec22eb6
Your Name <you@example.com>
2019-09-14 17:09:55 +0800

    update file

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,17 +1,22 @@
 const routes = [
   {
-    path: "/",
-    component: () => import("layouts/MyLayout.vue"),
-    children: [{ path: "", component: () => import("pages/Index.vue") }]
+    path: '/',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      // { path: '', component: () => import('pages/Index.vue') },
+      { path: '/user', component: () => import('pages/user.vue') },
+      { path: '/', component: () => import('pages/map.vue') },
+      { path: '/setting', component: () => import('pages/index.vue') }
+    ]
   }
-];
+]
 
 // Always leave this as last one
-if (process.env.MODE !== "ssr") {
+if (process.env.MODE !== 'ssr') {
   routes.push({
-    path: "*",
-    component: () => import("pages/Error404.vue")
-  });
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
 }
 
-export default routes;
+export default routes
