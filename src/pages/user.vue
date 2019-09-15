98fc14c6c1f36972dd279dd3b76e34a9adf067aa
Your Name <you@example.com>
2019-09-15 13:26:23 +0800

    fix percision

--- src/pages/user.vue
+++ src/pages/user.vue
@@ -164,7 +164,7 @@ export default {
     getPrice () {
       const park = this.parkInfo
       if (park && park.parkLotInfo) {
-        return park.parkLotInfo.current_price * 3.6
+        return (park.parkLotInfo.current_price * 3.6).toFixed(2)
       }
       return ''
     }
